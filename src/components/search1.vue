28a699a0ace786f1f1e4e1b704ed511f8c764444
frllk <1402805300@qq.com>
2020-07-04 14:06:07 +0800

    index1、search1

---
+++ src/components/search1.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- 搜索 -->
+  <view class="search">
+    <view class="sinput"><input type="text" placeholder="搜索"></view>
+    <!-- 搜索结果 -->
+    <view class="scontent"></view>
+  </view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.search {
+  display: flex;
+  flex-direction: column;
+  .sinput {
+    box-sizing: border-box;
+    padding: 20rpx 16rpx;
+    background: #ff2d4a;
+    position: relative;
+    input {
+      background-color: #fff;
+      flex: 1;
+      height: 60rpx;
+      line-height: 60rpx;
+      text-align: center;
+      font-size: 24rpx;
+      color: #bbb;
+      border-radius: 5rpx;
+    }
+  }
+}
+</style>
---
+++ src/pages/index/index1.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="index">
+    <!-- 搜索 -->
+    <!-- <view class="search"></view> -->
+    <search />
+    <!-- 轮播图 -->
+    <view class="slider"></view>
+    <!-- 功能导航 -->
+    <view class="navs"></view>
+    <!-- 栏目楼层 -->
+    <view class="floors"></view>
+    <!-- 底部提示 -->
+    <view class="end">
+      <text>我是有底线的！</text>
+    </view>
+  </view>
+</template>
+
+<script>
+import search from "@/components/search1.vue";
+export default {
+  components: {
+    search
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
