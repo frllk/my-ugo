53478861fe67cfdf06760a71710aa42bf4be8d54
frllk <1402805300@qq.com>
2020-07-04 10:41:15 +0800

    首页布局

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function() {
+    console.log("App Launch");
+  },
+  onShow: function() {
+    console.log("App Show");
+  },
+  onHide: function() {
+    console.log("App Hide");
+  }
+};
 </script>
 
-<style>
-	/*每个页面公共css */
+<style lang="scss">
+/*每个页面公共css=>全局样式 */
+image {
+  width: 100%;
+  height: 100%;
+  vertical-align: middle;
+}
+input {
+  display: block;
+  height: 100%;
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+}
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,192 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="index">
+    <!-- 搜索 -->
+    <view class="search">
+      <!-- 搜索区域 -->
+      <view class="sinput">
+        <input type="text" placeholder="搜索">
+      </view>
+      <!-- 搜索结果 -->
+      <view class="content"></view>
+    </view>
+    <!-- 轮播图 -->
+    <view class="silder">
+      <swiper indicator-dots autoplay circular indicator-color="rgba(0,0,0,.2)"
+        indicator-active-color="rgba(255,255,255,1)" interval="2000">
+        <swiper-item>
+          <navigator url="">
+            <image src="/static/uploads/banner1.png"></image>
+          </navigator>
+        </swiper-item>
+        <swiper-item>
+          <navigator url="">
+            <image src="/static/uploads/banner2.png"></image>
+          </navigator>
+        </swiper-item>
+        <swiper-item>
+          <navigator url="">
+            <image src="/static/uploads/banner3.png"></image>
+          </navigator>
+        </swiper-item>
+      </swiper>
+    </view>
+    <!-- 栏目导航 -->
+    <view class="navs">
+      <navigator url="">
+        <image src="/static/uploads/icon_index_nav_1@2x.png" />
+      </navigator>
+      <navigator url="">
+        <image src="/static/uploads/icon_index_nav_2@2x.png" />
+      </navigator>
+      <navigator url="">
+        <image src="/static/uploads/icon_index_nav_3@2x.png" />
+      </navigator>
+      <navigator url="">
+        <image src="/static/uploads/icon_index_nav_4@2x.png" />
+      </navigator>
+    </view>
+    <!-- 栏目floor1 -->
+    <view class="floor" wx:for="{{3}}" wx:key="index">
+      <!-- title -->
+      <view class="ftitle">
+        <image src="/static/uploads/pic_floor01_title.png" />
+      </view>
+      <!-- pics -->
+      <view class="fitem">
+        <navigator url>
+          <image src="/static/uploads/pic_floor01_1@2x.png" />
+        </navigator>
+        <navigator url>
+          <image src="/static/uploads/pic_floor01_2@2x.png" />
+        </navigator>
+        <navigator url>
+          <image src="/static/uploads/pic_floor01_3@2x.png" />
+        </navigator>
+        <navigator url>
+          <image src="/static/uploads/pic_floor01_4@2x.png" />
+        </navigator>
+        <navigator url>
+          <image src="/static/uploads/pic_floor01_5@2x.png" />
+        </navigator>
+      </view>
+    </view>
+    <!-- 底部提示 -->
+    <view class="end">
+      <text>我是有底线的</text>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello Ugo商城，我来了~~~'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      title: "Hello Ugo商城，我来了~~~"
+    };
+  },
+  onLoad() {},
+  methods: {}
+};
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
+<style lang="scss">
+// 搜索
+.search {
+  display: flex;
+  flex-direction: column;
+  .sinput {
+    padding: 20rpx 16rpx;
+    background: #ff2d4a;
+    position: relative;
+    // 伪类元素
+    &::after {
+      position: absolute;
+      top: 34rpx;
+      left: 302rpx;
+      content: "";
+      width: 32rpx;
+      height: 32rpx;
+      margin-right: 16rpx;
+      background-color: orange;
+      background: url("/static/images/icon_search@2x.png");
+      background-size: 32rpx;
+    }
+    input {
+      background: #fff;
+      height: 60rpx;
+      line-height: 60rpx;
+      text-align: center;
+      font-size: 24rpx;
+      color: #bbbbbb;
+      border-radius: 5rpx;
+    }
+  }
+}
+// 轮播图
+.silder {
+  .swiper,
+  image {
+    width: 100%;
+    height: 340rpx;
+  }
+}
+// 栏目导航
+.navs {
+  display: flex;
+  margin: 24rpx 0 29rpx 0;
+  justify-content: space-around;
+  background: #fff;
+  image {
+    width: 128rpx;
+    height: 140rpx;
+  }
+}
+// 栏目楼层
+.floor {
+  .ftitle {
+    padding-top: 30rpx;
+    background: #f4f4f4;
+    image {
+      width: 750rpx;
+      height: 60rpx;
+    }
+  }
+  // pics
+  .fitem {
+    padding: 20rpx 16rpx 10rpx;
+    overflow: hidden;
+    navigator {
+      float: left;
+      width: 193rpx;
+      height: 188rpx;
+      margin-left: 10rpx;
+      margin-bottom: 10rpx;
+    }
+    navigator:nth-child(1) {
+      width: 232rpx;
+      height: 386rpx;
+      margin-left: 0;
+    }
+    navigator:nth-child(2),
+    navigator:nth-child(5) {
+      width: 273rpx;
+      height: 188rpx;
+    }
+  }
+  &:first-child {
+    .fitem {
+      navigator {
+        width: 233rpx;
+      }
+    }
+  }
+}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+// 底部提示
+.end {
+  padding: 30rpx;
+  text-align: center;
+  font-size: 24rpx;
+  color: #999;
+}
 </style>
